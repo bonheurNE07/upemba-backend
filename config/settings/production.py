7a5bb7b1bd1ee3f854531e4ae72c3538cedbe7f5
bonheurNE07 <bonheurndezenc@gmail.com>
2026-03-21 23:25:26 +0200

    build: Decouple strict HTTPS Cookie enforcement to allow raw edge IP logins on the Raspberry Pi

--- config/settings/production.py
+++ config/settings/production.py
@@ -38,14 +38,15 @@ CACHES = {
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
-# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-SESSION_COOKIE_SECURE = True
-# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
-SESSION_COOKIE_NAME = "__Secure-sessionid"
-# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-CSRF_COOKIE_SECURE = True
-# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-name
-CSRF_COOKIE_NAME = "__Secure-csrftoken"
+
+# Fallback: if disabled for raw Edge IoT networks, strip explicit HTTPS browser restrictions
+SESSION_COOKIE_SECURE = SECURE_SSL_REDIRECT
+CSRF_COOKIE_SECURE = SECURE_SSL_REDIRECT
+if SECURE_SSL_REDIRECT:
+    SESSION_COOKIE_NAME = "__Secure-sessionid"
+    CSRF_COOKIE_NAME = "__Secure-csrftoken"
+
+CSRF_TRUSTED_ORIGINS = [f"http://{host}" for host in ALLOWED_HOSTS] + [f"https://{host}" for host in ALLOWED_HOSTS]
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
